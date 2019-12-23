75197be990e5f5c0c1c7bc244751886f0d5569c3
justin E Mozader <zader64@yahoo.com>
2019-12-22 21:42:36 -0500

    First commit

---
+++ GSF/Init.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch;
+using Torch.API;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+
+
+namespace GSF
+{
+    public class OS : TorchPluginBase
+    {
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+            Log.Info($"Torch Init Complete:{DateTime.Now}");
+        }
+
+        [Category("torch")]
+        public class commands :CommandModule
+        {
+            public OS Plugin => (OS)Context.Plugin;
+
+            [Command("Reset", "Resets the server in 1 min")]
+            [Permission(MyPromoteLevel.None)]
+            public void Reset(string trigger, string bar = null)
+            {
+                Log.Info("This is a Test from " + Context.Player);
+            }
+        }
+    }
+}
